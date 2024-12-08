bbda5ca7a3901933b58e5fe6c955d14647053154
Desmondxx2000 <desmondbrown20000@gmail.com>
2024-12-07 23:06:07 -0500

    Initial files for MakeCode project

---
+++ main.py
@@ -0,0 +1,2 @@
+music.play(music.string_playable("E B C5 A B G A F ", 190),
+    music.PlaybackMode.IN_BACKGROUND)
---
+++ main.ts
@@ -0,0 +1,1 @@
+music.play(music.stringPlayable("E B C5 A B G A F ", 190), music.PlaybackMode.InBackground)
